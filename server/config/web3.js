92797c8a0819671d2f8960c110f417612eeec491
Tahseen Malik <hello@tahseen.me>
2018-05-29 10:57:40 +0100

    feat: Use ENV var for HTTP provider in production

--- server/config/web3.js
+++ server/config/web3.js
@@ -1,8 +1,11 @@
 'use strict'
 const Web3 = require('web3')
+const { isDevelopment } = require('../utils')
 
-/** @TODO: Consume from config or ENV variable */
-const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/RE5yuTqXCfgmv4Z0xFLu')
+const url = isDevelopment()
+  ? 'https://rinkeby.infura.io/RE5yuTqXCfgmv4Z0xFLu'
+  : process.env.WEB3_HTTP_PROVIDER_URL
+const provider = new Web3.providers.HttpProvider(url)
 const web3 = new Web3(provider)
 
 module.exports = { web3 }
