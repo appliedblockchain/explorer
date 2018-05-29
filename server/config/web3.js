75da1960927375461839e2956d0f997ca1964194
Tahseen Malik <hello@tahseen.me>
2018-05-29 11:01:20 +0100

    feat: Use ETHEREUM_JSON_ENDPOINT

--- server/config/web3.js
+++ server/config/web3.js
@@ -4,7 +4,7 @@ const { isDevelopment } = require('../utils')
 
 const url = isDevelopment()
   ? 'https://rinkeby.infura.io/RE5yuTqXCfgmv4Z0xFLu'
-  : process.env.WEB3_HTTP_PROVIDER_URL
+  : process.env.ETHEREUM_JSON_ENDPOINT
 const provider = new Web3.providers.HttpProvider(url)
 const web3 = new Web3(provider)
 
